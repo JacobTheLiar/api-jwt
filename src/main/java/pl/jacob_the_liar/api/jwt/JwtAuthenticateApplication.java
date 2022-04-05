7a8695c472aa6c9cda4f3ec1933cf476d10da824
jakub <jacob_the_liar@tlen.pl>
2022-04-05 19:05:35 +0200

    Initial commit

---
+++ src/main/java/pl/jacob_the_liar/api/jwt/JwtAuthenticateApplication.java
@@ -0,0 +1,14 @@
+package pl.jacob_the_liar.api.jwt;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JwtAuthenticateApplication{
+	
+	public static void main(String[] args){
+		SpringApplication.run(JwtAuthenticateApplication.class, args);
+	}
+}
